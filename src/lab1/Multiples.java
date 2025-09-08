8c3bc2903e58f607b71320dab65b4a7ab474fb92
Sushaan Patel <sushaanpatel@gmail.com>
2025-09-08 10:31:05 -0400

    adding files for task 4

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,12 +2,18 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        for (int i = 1; i < 1000; i++){
-            boolean divBy3 = (i % 3 == 0);
-            boolean divBy5 = (i % 5 == 0);
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int out = 0;
+        for (int i = 1; i < n; i++){
+            boolean divBy3 = (i % a == 0);
+            boolean divBy5 = (i % b == 0);
             if (divBy5 || divBy3){
-                System.out.println(i);
+                out = out + 1;
             }
         }
+        return out;
     }
 }
